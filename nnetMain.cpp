34280cfb2507df7202c11e6fee679e3d472ca418
阿邦 <windwaildd@gmail.com>
2015-06-23 16:31:19 +0800

    Update nnetMain.cpp

--- nnetMain.cpp
+++ nnetMain.cpp
@@ -7,14 +7,12 @@
 #define XPU GPU
 
 DEFINE_string (config, "config/imagenet56_conv.cfg", "config file");
-DEFINE_int32  (gpu, 0, "gpu id");
 
 int main (int argc, char** argv)
 { fLB::FLAGS_colorlogtostderr = true;
   google::ParseCommandLineFlags (&argc, &argv, true);
   GLogHelper glogh (argv[0], "/home/liuminxu/log/nnetMain.");
 
-//cuda_set_p2p ();
 //omp_set_num_threads (4);
 //mkl_set_num_threads (4);
 
@@ -22,16 +20,17 @@ int main (int argc, char** argv)
   NNetModel<XPU> model;
   model.para_.config (cfg);
 
-  dnnctx.resize (model.para_.num_device);
-  for (int i = 0; i < model.para_.num_device; ++i)
+  dnnctx.resize (model.para_.num_nnets);
+  for (int i = model.para_.min_device; i <= model.para_.max_device; ++i)
   { dnnctx[i] = new XPUCtx (i);
     dnnctx[i]->reset ();
   }
+//cuda_set_p2p (model.para_.num_device);
 
   model.init ();
   model.train ();
 
-//cuda_del_p2p ();
+//cuda_del_p2p (model.para_.num_device);
 //for (int i = 0; i < CUDA_NUM_DEVICE; ++i)
 //  delete dnnctx[i];
 }
