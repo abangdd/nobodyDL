2e1535f169d9482a136650ee41b42955379f6edf
阿邦 <windwaildd@gmail.com>
2015-08-14 18:58:35 +0800

    Update nnetDropout.cpp

--- nnetDropout.cpp
+++ nnetDropout.cpp
@@ -23,7 +23,7 @@ XPU_KERNEL(DropoutBackward) (const int num_kernels, const DT *dst, const DT *mas
 
 LAYER_FORWARD (LayerDropout)
 { const int N = dst_.size();
-  if (is_train)
+  if (is_train && drop_ >= 0.1f)
   { mask.init (rand_, UNIFORM, 0.f, 1.f);
     XPU_KERNEL_LAUNCH (DropoutForward,  cuda_get_blocks(N), CUDA_NUM_THREADS, 0, CUDNN_STREAM,
       N, src_.dptr, mask.dptr, drop_, scal_, dst_.dptr);
@@ -34,11 +34,12 @@ LAYER_FORWARD (LayerDropout)
 
 LAYER_BACKPROP (LayerDropout)
 { const int N = dst_.size();
-  if (is_prop_grad)
+  if (is_prop_grad && drop_ >= 0.1f)
   { XPU_KERNEL_LAUNCH (DropoutBackward, cuda_get_blocks(N), CUDA_NUM_THREADS, 0, CUDNN_STREAM,
       N, dst_.dptr, mask.dptr, drop_, scal_, src_.dptr);
     cuda_sync_check ("DropoutBackward");
-  }
+  } else
+    src_.copy (dst_);
 }
 
 LAYER_INIT (LayerDropout)
