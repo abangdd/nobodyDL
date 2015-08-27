fe9db145844d99f9fa6056e6f9de092520d38f7a
阿邦 <windwaildd@gmail.com>
2015-08-28 01:31:04 +0800

    Update optimVSGD.cpp

--- optimVSGD.cpp
+++ optimVSGD.cpp
@@ -5,11 +5,9 @@
 
 template <typename XPU, typename DT>
 OptimVSGD<XPU, DT>::OptimVSGD (ParaOptim &po, const int did, Tensor<XPU, DT> &weight, Tensor<XPU, DT> &wgrad) :
-  OptimBase<XPU, DT> (po, did, weight, wgrad), para_(po), did_(did), wmat_(weight), gmat_(wgrad), epoch(0)
+  OptimBase<XPU, DT> (po, did, weight, wgrad), po_(po), did_(did), wmat_(weight), gmat_(wgrad), epoch(0)
 { mmat_.create (wmat_.shape, did_);  mmat_.mem_set (0);
   hmat_.create (wmat_.shape, did_);  hmat_.mem_set (0);
-if (para_.algo == 2)
-  vmat_.create (wmat_.shape, did_);  vmat_.mem_set (0);
 }
 #ifdef __CUDACC__
 template OptimVSGD<GPU, float >::OptimVSGD (ParaOptim &po, const int did, TensorGPUf &weight, TensorGPUf &wgrad);
@@ -21,20 +19,18 @@ template OptimVSGD<CPU, double>::OptimVSGD (ParaOptim &po, const int did, Tensor
 
 template <typename XPU, typename DT>
 void OptimVSGD<XPU, DT>::update ()
-{ if (para_.algo == 0)
+{ if (po_.algo == 0)
     update_sgd ();
-  if (para_.algo == 1)
+  if (po_.algo == 1)
     update_nag ();
-  if (para_.algo == 2)
-    update_adam ();
 }
 
 template <typename XPU, typename DT>
 void OptimVSGD<XPU, DT>::update_sgd ()
 { // mmat_ = momentum * mmat_ - lr * (gmat_ + wd * wmat_)
-  gmat_.blas_axpy (wmat_, para_.wd);
-  mmat_.blas_scal (para_.momentum);
-  mmat_.blas_axpy (gmat_, - para_.lrate);
+  gmat_.blas_axpy (wmat_, po_.wd);
+  mmat_.blas_scal (po_.momentum);
+  mmat_.blas_axpy (gmat_, - po_.lrate);
 
   wmat_.blas_vadd (wmat_, mmat_);
 }
@@ -43,33 +39,12 @@ template <typename XPU, typename DT>
 void OptimVSGD<XPU, DT>::update_nag ()
 { hmat_.copy (mmat_);
 
-  gmat_.blas_axpy (wmat_, para_.wd);
-  mmat_.blas_scal (para_.momentum);
-  mmat_.blas_axpy (gmat_, - para_.lrate);
+  gmat_.blas_axpy (wmat_, po_.wd);
+  mmat_.blas_scal (po_.momentum);
+  mmat_.blas_axpy (gmat_, - po_.lrate);
 
-  wmat_.blas_axpy (hmat_, - para_.momentum);
-  wmat_.blas_axpy (mmat_, 1+para_.momentum);
-}
-
-template <typename XPU, typename DT>
-void OptimVSGD<XPU, DT>::update_adam ()
-{ gmat_.blas_axpy (wmat_, para_.wd);
-  hmat_.blas_vsqr (gmat_);
-
-  mmat_.blas_scal (0.9f);
-  mmat_.blas_axpy (gmat_, 0.1f);
-  vmat_.blas_scal (0.999f);
-  vmat_.blas_axpy (hmat_, 0.001f);
-
-  hmat_.blas_vsqrt(vmat_);  hmat_.add (1e-8f);
-  hmat_.blas_vdiv (mmat_, hmat_);
-
-  DT fix1 = 1.f - powf (0.9f,   epoch + 1);
-  DT fix2 = 1.f - powf (0.999f, epoch + 1);
-  DT lr_t = para_.lr_alpha * para_.lrate * sqrt(fix2) / fix1;
-  epoch++;
-
-  wmat_.blas_axpy (hmat_, - lr_t);
+  wmat_.blas_axpy (hmat_, - po_.momentum);
+  wmat_.blas_axpy (mmat_, 1+po_.momentum);
 }
 
 template <typename XPU, typename DT>
