6ab09d00f8a2bc8815d86621c404819576401cd2
阿邦 <windwaildd@gmail.com>
2015-08-30 11:27:59 +0800

    Update nnetDropout.cpp

--- nnetDropout.cpp
+++ nnetDropout.cpp
@@ -10,42 +10,34 @@ template LayerDropout<CPU>::LayerDropout (ParaLayer &pl, const int did, TensorCP
 #endif
 
 template <typename DT>
-XPU_KERNEL(DropoutForward)  (const int num_kernels, const DT *src, const DT *mask, DT threshold, const DT scale, DT *dst)
+XPU_KERNEL(kernel_dropout) (const int num_kernels, const DT *mask, DT threshold, const DT scale, DT *data)
 { kernel_for (i, num_kernels)
-    dst[i] = src[i] * (mask[i] > threshold) * scale;
-}
-
-template <typename DT>
-XPU_KERNEL(DropoutBackward) (const int num_kernels, const DT *dst, const DT *mask, DT threshold, const DT scale, DT *src)
-{ kernel_for (i, num_kernels)
-    src[i] = dst[i] * (mask[i] > threshold) * scale;
+    data[i] *= (mask[i] > threshold) * scale;
 }
 
 LAYER_FORWARD (LayerDropout)
-{ scal_ = 1. / (1. - pl_.dropout);
+{ scal_ = 1 / (1 - pl_.dropout);
   const int N = dst_.size();
   if (is_train && pl_.dropout > 0.01)
   { mask.init (rand_, UNIFORM, 0.f, 1.f);
-    XPU_KERNEL_LAUNCH (DropoutForward,  cuda_get_blocks(N), CUDA_NUM_THREADS, 0, CUDNN_STREAM,
-      N, src_.dptr, mask.dptr, pl_.dropout, scal_, dst_.dptr);
+    XPU_KERNEL_LAUNCH (kernel_dropout, cuda_get_blocks(N), CUDA_NUM_THREADS, 0, CUDNN_STREAM,
+      N, mask.dptr, pl_.dropout, scal_, dst_.dptr);
     cuda_sync_check ("DropoutForward");
-  } else
-    dst_.copy (src_);
+  }
 }
 
 LAYER_BACKPROP (LayerDropout)
-{ scal_ = 1. / (1. - pl_.dropout);
-  const int N = dst_.size();
+{ scal_ = 1 / (1 - pl_.dropout);
+  const int N = src_.size();
   if (is_prop_grad && pl_.dropout > 0.01)
-  { XPU_KERNEL_LAUNCH (DropoutBackward, cuda_get_blocks(N), CUDA_NUM_THREADS, 0, CUDNN_STREAM,
-      N, dst_.dptr, mask.dptr, pl_.dropout, scal_, src_.dptr);
+  { XPU_KERNEL_LAUNCH (kernel_dropout, cuda_get_blocks(N), CUDA_NUM_THREADS, 0, CUDNN_STREAM,
+      N, mask.dptr, pl_.dropout, scal_, src_.dptr);
     cuda_sync_check ("DropoutBackward");
-  } else
-    src_.copy (dst_);
+  }
 }
 
 LAYER_INIT (LayerDropout)
-{ dst_.create (src_.shape, did_);
+{ dst_ = src_;
   mask.create (src_.shape, did_);
 }
 
