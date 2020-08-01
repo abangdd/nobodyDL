6d75da5eb53a71eaf7c23fcc31d1c830372144e6
阿邦 <windwaildd@gmail.com>
2020-08-02 00:01:51 +0800

    Update tensorBLAS.cpp

--- tensor/tensorBLAS.cpp
+++ tensor/tensorBLAS.cpp
@@ -4,75 +4,54 @@
 #include "../include/tensor.h"
 
 #ifdef __CUDACC__
-inline cublasOperation_t cublas_get_trans (bool t)  { return t ? CUBLAS_OP_T : CUBLAS_OP_N;  }
+inline cublasOperation_t cublas_get_trans (bool t) { return t ? CUBLAS_OP_T : CUBLAS_OP_N; }
 #else
-inline CBLAS_TRANSPOSE    cblas_get_trans (bool t)  { return t ? CblasTrans : CblasNoTrans;  }
-#endif
-
-
-#ifdef __CUDACC__
-template <>
-void TensorGPUf::blas_amax (int &idx, float &val) const
-{ const int N = size();
-  cuda_check (
-  cublasIsamax (get_blas_handle(), N, dptr, 1, &idx));
-  idx = idx - 1;
-  val = dptr[idx];
-};
-template <>
-void TensorGPUd::blas_amax (int &idx, double &val) const
-{ const int N = size();
-  cuda_check (
-  cublasIdamax (get_blas_handle(), N, dptr, 1, &idx));
-  idx = idx - 1;
-  val = dptr[idx];
-};
-#else
-template <>
-void TensorCPUf::blas_amax (int &idx, float  &val) const
-{ const int N = size();
-  idx = cblas_isamax (N, dptr, 1);
-  val = dptr[idx];
-};
-template <>
-void TensorCPUd::blas_amax (int &idx, double &val) const
-{ const int N = size();
-  idx = cblas_idamax (N, dptr, 1);
-  val = dptr[idx];
-};
+inline CBLAS_TRANSPOSE    cblas_get_trans (bool t) { return t ? CblasTrans : CblasNoTrans; }
 #endif
 
 
 
 #ifdef __CUDACC__
 template <>
-void TensorGPUf::blas_amin (int &idx, float &val) const
-{ const int N = size();
-  cuda_check (
-  cublasIsamin (get_blas_handle(), N, dptr, 1, &idx));
-  idx = idx - 1;
-  val = dptr[idx];
+void TensorGPUf::blas_gemm (const bool transA, const bool transB, const TensorGPUf& A, const TensorGPUf& B, float alpha, float beta) {
+    const int M = (!transA) ? A.rows() : A.cols();    CHECK_EQ (M, rows());
+    const int N = (!transB) ? B.cols() : B.rows();    CHECK_EQ (N, cols());
+    const int K = (!transA) ? A.cols() : A.rows();
+    const int lda = (!transA) ? K : M;    // A.cols() : A.rows();
+    const int ldb = (!transB) ? N : K;    // B.cols() : B.rows();
+    cuda_check (cublasSgemm (get_blas_handle(), cublas_get_trans (transB), cublas_get_trans (transA), N, M, K,
+        &alpha, B.dptr, ldb, A.dptr, lda, &beta, dptr, N));
 };
 template <>
-void TensorGPUd::blas_amin (int &idx, double &val) const
-{ const int N = size();
-  cuda_check (
-  cublasIdamin (get_blas_handle(), N, dptr, 1, &idx));
-  idx = idx - 1;
-  val = dptr[idx];
+void TensorGPUd::blas_gemm (const bool transA, const bool transB, const TensorGPUd& A, const TensorGPUd& B, double alpha, double beta) {
+    const int M = (!transA) ? A.rows() : A.cols();    CHECK_EQ (M, rows());
+    const int N = (!transB) ? B.cols() : B.rows();    CHECK_EQ (N, cols());
+    const int K = (!transA) ? A.cols() : A.rows();
+    const int lda = (!transA) ? K : M;
+    const int ldb = (!transB) ? N : K;
+    cuda_check (cublasDgemm (get_blas_handle(), cublas_get_trans (transB), cublas_get_trans (transA), N, M, K,
+        &alpha, B.dptr, ldb, A.dptr, lda, &beta, dptr, N));
 };
 #else
 template <>
-void TensorCPUf::blas_amin (int &idx, float  &val) const
-{ const int N = size();
-  idx = cblas_isamin (N, dptr, 1);
-  val = dptr[idx];
+void TensorCPUf::blas_gemm (const bool transA, const bool transB, const TensorCPUf& A, const TensorCPUf& B, float alpha, float beta) {
+    const int M = (!transA) ? A.rows() : A.cols();    CHECK_EQ (M, rows());
+    const int N = (!transB) ? B.cols() : B.rows();    CHECK_EQ (N, cols());
+    const int K = (!transA) ? A.cols() : A.rows();
+    const int lda = (!transA) ? K : M;
+    const int ldb = (!transB) ? N : K;
+    cblas_sgemm (CblasRowMajor, cblas_get_trans (transA), cblas_get_trans (transB), M, N, K,
+        alpha, A.dptr, lda, B.dptr, ldb, beta, dptr, N);
 };
 template <>
-void TensorCPUd::blas_amin (int &idx, double &val) const
-{ const int N = size();
-  idx = cblas_idamin (N, dptr, 1);
-  val = dptr[idx];
+void TensorCPUd::blas_gemm (const bool transA, const bool transB, const TensorCPUd& A, const TensorCPUd& B, double alpha, double beta) {
+    const int M = (!transA) ? A.rows() : A.cols();    CHECK_EQ (M, rows());
+    const int N = (!transB) ? B.cols() : B.rows();    CHECK_EQ (N, cols());
+    const int K = (!transA) ? A.cols() : A.rows();
+    const int lda = (!transA) ? K : M;
+    const int ldb = (!transB) ? N : K;
+    cblas_dgemm (CblasRowMajor, cblas_get_trans (transA), cblas_get_trans (transB), M, N, K,
+        alpha, A.dptr, lda, B.dptr, ldb, beta, dptr, N);
 };
 #endif
 
@@ -80,27 +59,41 @@ void TensorCPUd::blas_amin (int &idx, double &val) const
 
 #ifdef __CUDACC__
 template <>
-void TensorGPUf::blas_asum (float  &val) const
-{ const int N = size();
-  cuda_check (
-  cublasSasum (get_blas_handle(), N, dptr, 1, &val));
+void TensorGPUf::blas_gemv (const bool transA, const TensorGPUf& A, const TensorGPUf& X, float alpha, float beta) {
+    const int M = A.rows();
+    const int N = A.cols();
+    const int lda = N;
+    CHECK_EQ (transA ? N:M, rows());
+    CHECK_EQ (size(), rows());
+    cuda_check (cublasSgemv (get_blas_handle(), cublas_get_trans (!transA), N, M, &alpha, A.dptr, lda, X.dptr, 1, &beta, dptr, 1));
 };
 template <>
-void TensorGPUd::blas_asum (double &val) const
-{ const int N = size();
-  cuda_check (
-  cublasDasum (get_blas_handle(), N, dptr, 1, &val));
+void TensorGPUd::blas_gemv (const bool transA, const TensorGPUd& A, const TensorGPUd& X, double alpha, double beta) {
+    const int M = A.rows();
+    const int N = A.cols();
+    const int lda = N;
+    CHECK_EQ (transA ? N:M, rows());
+    CHECK_EQ (size(), rows());
+    cuda_check (cublasDgemv (get_blas_handle(), cublas_get_trans (!transA), N, M, &alpha, A.dptr, lda, X.dptr, 1, &beta, dptr, 1));
 };
 #else
 template <>
-void TensorCPUf::blas_asum (float  &val) const
-{ const int N = size();
-  val = cblas_sasum (N, dptr, 1);
+void TensorCPUf::blas_gemv (const bool transA, const TensorCPUf& A, const TensorCPUf& X, float alpha, float beta) {
+    const int M = A.rows();
+    const int N = A.cols();
+    const int lda = N;
+    CHECK_EQ (transA ? N:M, rows());
+    CHECK_EQ (size(), rows());
+    cblas_sgemv (CblasRowMajor, cblas_get_trans ( transA), M, N, alpha, A.dptr, lda, X.dptr, 1, beta, dptr, 1);
 };
 template <>
-void TensorCPUd::blas_asum (double &val) const
-{ const int N = size();
-  val = cblas_dasum (N, dptr, 1);
+void TensorCPUd::blas_gemv (const bool transA, const TensorCPUd& A, const TensorCPUd& X, double alpha, double beta) {
+    const int M = A.rows();
+    const int N = A.cols();
+    const int lda = N;
+    CHECK_EQ (transA ? N:M, rows());
+    CHECK_EQ (size(), rows());
+    cblas_dgemv (CblasRowMajor, cblas_get_trans ( transA), M, N, alpha, A.dptr, lda, X.dptr, 1, beta, dptr, 1);
 };
 #endif
 
@@ -108,27 +101,45 @@ void TensorCPUd::blas_asum (double &val) const
 
 #ifdef __CUDACC__
 template <>
-void TensorGPUf::blas_axpy (const TensorGPUf &in, float  alpha)
-{ const int N = size();  CHECK_EQ (N, in.size());
-  cuda_check (
-  cublasSaxpy (get_blas_handle(), N, &alpha, in.dptr, 1, dptr, 1));
+float  TensorGPUf::blas_asum () const {
+    float val;
+    cuda_check (cublasSasum (get_blas_handle(), size(), dptr, 1, &val));
+    return val;
 };
 template <>
-void TensorGPUd::blas_axpy (const TensorGPUd &in, double alpha)
-{ const int N = size();  CHECK_EQ (N, in.size());
-  cuda_check (
-  cublasDaxpy (get_blas_handle(), N, &alpha, in.dptr, 1, dptr, 1));
+double TensorGPUd::blas_asum () const {
+    double val;
+    cuda_check (cublasDasum (get_blas_handle(), size(), dptr, 1, &val));
+    return val;
+};
+template <>
+float  TensorGPUf::blas_nrm2 () const {
+    float val;
+    cuda_check (cublasSnrm2 (get_blas_handle(), size(), dptr, 1, &val));
+    return val;
+};
+template <>
+double TensorGPUd::blas_nrm2 () const {
+    double val;
+    cuda_check (cublasDnrm2 (get_blas_handle(), size(), dptr, 1, &val));
+    return val;
 };
 #else
 template <>
-void TensorCPUf::blas_axpy (const TensorCPUf &in, float  alpha)
-{ const int N = size();  CHECK_EQ (N, in.size());
-  cblas_saxpy (N, alpha, in.dptr, 1, dptr, 1);
+float  TensorCPUf::blas_asum () const {
+    return cblas_sasum (size(), dptr, 1);
 };
 template <>
-void TensorCPUd::blas_axpy (const TensorCPUd &in, double alpha)
-{ const int N = size();  CHECK_EQ (N, in.size());
-  cblas_daxpy (N, alpha, in.dptr, 1, dptr, 1);
+double TensorCPUd::blas_asum () const {
+    return cblas_dasum (size(), dptr, 1);
+};
+template <>
+float  TensorCPUf::blas_nrm2 () const {
+    return cblas_snrm2 (size(), dptr, 1);
+};
+template <>
+double TensorCPUd::blas_nrm2 () const {
+    return cblas_dnrm2 (size(), dptr, 1);
 };
 #endif
 
@@ -136,27 +147,57 @@ void TensorCPUd::blas_axpy (const TensorCPUd &in, double alpha)
 
 #ifdef __CUDACC__
 template <>
-void TensorGPUf::blas_copy_from (const float *x, const int incx, const int incy)
-{ const int N = size();
-  cuda_check (
-  cublasScopy (get_blas_handle(), N, x, incx, dptr, incy));
+void TensorGPUf::blas_amax (int& idx, float& val) const {
+    const int N = size();
+    cuda_check (cublasIsamax (get_blas_handle(), N, dptr, 1, &idx));
+    idx = idx - 1;
+    val = dptr[idx];
 };
 template <>
-void TensorGPUf::blas_copy_to   (float *x, const int incx, const int incy) const
-{ const int N = size();
-  cuda_check (
-  cublasScopy (get_blas_handle(), N, dptr, incy, x, incx));
+void TensorGPUd::blas_amax (int& idx, double& val) const {
+    const int N = size();
+    cuda_check (cublasIdamax (get_blas_handle(), N, dptr, 1, &idx));
+    idx = idx - 1;
+    val = dptr[idx];
+};
+template <>
+void TensorGPUf::blas_amin (int& idx, float& val) const {
+    const int N = size();
+    cuda_check (cublasIsamin (get_blas_handle(), N, dptr, 1, &idx));
+    idx = idx - 1;
+    val = dptr[idx];
+};
+template <>
+void TensorGPUd::blas_amin (int& idx, double& val) const {
+    const int N = size();
+    cuda_check (cublasIdamin (get_blas_handle(), N, dptr, 1, &idx));
+    idx = idx - 1;
+    val = dptr[idx];
 };
 #else
 template <>
-void TensorCPUf::blas_copy_from (const float *x, const int incx, const int incy)
-{ const int N = size();
-  cblas_scopy (N, x, incx, dptr, incy);
+void TensorCPUf::blas_amax (int& idx, float& val) const {
+    const int N = size();
+    idx = cblas_isamax (N, dptr, 1);
+    val = dptr[idx];
 };
 template <>
-void TensorCPUf::blas_copy_to   (float *x, const int incx, const int incy) const
-{ const int N = size();
-  cblas_scopy (N, dptr, incy, x, incx);
+void TensorCPUd::blas_amax (int& idx, double& val) const {
+    const int N = size();
+    idx = cblas_idamax (N, dptr, 1);
+    val = dptr[idx];
+};
+template <>
+void TensorCPUf::blas_amin (int& idx, float& val) const {
+    const int N = size();
+    idx = cblas_isamin (N, dptr, 1);
+    val = dptr[idx];
+};
+template <>
+void TensorCPUd::blas_amin (int& idx, double& val) const {
+    const int N = size();
+    idx = cblas_idamin (N, dptr, 1);
+    val = dptr[idx];
 };
 #endif
 
@@ -164,27 +205,45 @@ void TensorCPUf::blas_copy_to   (float *x, const int incx, const int incy) const
 
 #ifdef __CUDACC__
 template <>
-void TensorGPUf::blas_sdot  (const TensorGPUf &in, float  &val) const
-{ const int N = size();  CHECK_EQ (N, in.size());
-  cuda_check (
-  cublasSdot (get_blas_handle(), N, in.dptr, 1, dptr, 1, &val));
+void TensorGPUf::blas_axpy (const TensorGPUf& in, float alpha) {
+    CHECK_EQ (size(), in.size());
+    cuda_check (cublasSaxpy (get_blas_handle(), size(), &alpha, in.dptr, 1, dptr, 1));
 };
 template <>
-void TensorGPUd::blas_sdot  (const TensorGPUd &in, double &val) const
-{ const int N = size();  CHECK_EQ (N, in.size());
-  cuda_check (
-  cublasDdot (get_blas_handle(), N, in.dptr, 1, dptr, 1, &val));
+void TensorGPUd::blas_axpy (const TensorGPUd& in, double alpha) {
+    CHECK_EQ (size(), in.size());
+    cuda_check (cublasDaxpy (get_blas_handle(), size(), &alpha, in.dptr, 1, dptr, 1));
+};
+template <>
+void TensorGPUf::blas_sdot (const TensorGPUf& in, float& val) const {
+    CHECK_EQ (size(), in.size());
+    cuda_check (cublasSdot (get_blas_handle(), size(), in.dptr, 1, dptr, 1, &val));
+};
+template <>
+void TensorGPUd::blas_sdot (const TensorGPUd& in, double& val) const {
+    CHECK_EQ (size(), in.size());
+    cuda_check (cublasDdot (get_blas_handle(), size(), in.dptr, 1, dptr, 1, &val));
 };
 #else
 template <>
-void TensorCPUf::blas_sdot  (const TensorCPUf &in, float  &val) const
-{ const int N = size();  CHECK_EQ (N, in.size());
-  val = cblas_sdot (N, in.dptr, 1, dptr, 1);
+void TensorCPUf::blas_axpy (const TensorCPUf& in, float alpha) {
+    CHECK_EQ (size(), in.size());
+    cblas_saxpy (size(), alpha, in.dptr, 1, dptr, 1);
 };
 template <>
-void TensorCPUd::blas_sdot  (const TensorCPUd &in, double &val) const
-{ const int N = size();  CHECK_EQ (N, in.size());
-  val = cblas_ddot (N, in.dptr, 1, dptr, 1);
+void TensorCPUd::blas_axpy (const TensorCPUd& in, double alpha) {
+    CHECK_EQ (size(), in.size());
+    cblas_daxpy (size(), alpha, in.dptr, 1, dptr, 1);
+};
+template <>
+void TensorCPUf::blas_sdot (const TensorCPUf& in, float& val) const {
+    CHECK_EQ (size(), in.size());
+    val = cblas_sdot (size(), in.dptr, 1, dptr, 1);
+};
+template <>
+void TensorCPUd::blas_sdot (const TensorCPUd& in, double& val) const {
+    CHECK_EQ (size(), in.size());
+    val = cblas_ddot (size(), in.dptr, 1, dptr, 1);
 };
 #endif
 
@@ -192,55 +251,21 @@ void TensorCPUd::blas_sdot  (const TensorCPUd &in, double &val) const
 
 #ifdef __CUDACC__
 template <>
-void TensorGPUf::blas_nrm2 (float  &val) const
-{ const int N = size();
-  cuda_check (
-  cublasSnrm2 (get_blas_handle(), N, dptr, 1, &val));
+void TensorGPUf::blas_scal (float alpha) {
+    cuda_check (cublasSscal (get_blas_handle(), size(), &alpha, dptr, 1));
 };
 template <>
-void TensorGPUd::blas_nrm2 (double &val) const
-{ const int N = size();
-  cuda_check (
-  cublasDnrm2 (get_blas_handle(), N, dptr, 1, &val));
+void TensorGPUd::blas_scal (double alpha) {
+    cuda_check (cublasDscal (get_blas_handle(), size(), &alpha, dptr, 1));
 };
 #else
 template <>
-void TensorCPUf::blas_nrm2 (float  &val) const
-{ const int N = size();
-  val = cblas_snrm2 (N, dptr, 1);
+void TensorCPUf::blas_scal (float alpha) {
+    cblas_sscal (size(), alpha, dptr, 1);
 };
 template <>
-void TensorCPUd::blas_nrm2 (double &val) const
-{ const int N = size();
-  val = cblas_dnrm2 (N, dptr, 1);
-};
-#endif
-
-
-
-#ifdef __CUDACC__
-template <>
-void TensorGPUf::blas_scal (float  alpha)
-{ const int N = size();
-  cuda_check (
-  cublasSscal (get_blas_handle(), N, &alpha, dptr, 1));
-};
-template <>
-void TensorGPUd::blas_scal (double alpha)
-{ const int N = size();
-  cuda_check (
-  cublasDscal (get_blas_handle(), N, &alpha, dptr, 1));
-};
-#else
-template <>
-void TensorCPUf::blas_scal (float  alpha)
-{ const int N = size();
-  cblas_sscal (N, alpha, dptr, 1);
-};
-template <>
-void TensorCPUd::blas_scal (double alpha)
-{ const int N = size();
-  cblas_dscal (N, alpha, dptr, 1);
+void TensorCPUd::blas_scal (double alpha) {
+    cblas_dscal (size(), alpha, dptr, 1);
 };
 #endif
 
