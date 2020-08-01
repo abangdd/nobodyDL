576bc649aa3cf746eb3341f0422bbfb85e471ef1
阿邦 <windwaildd@gmail.com>
2020-08-01 23:47:32 +0800

    Update nnetLoss.cpp

--- nnet/nnetLoss.cpp
+++ nnet/nnetLoss.cpp
@@ -2,93 +2,92 @@
 #define NNET_LOSS_
 
 #include "../include/nnet.h"
-using std::max;
-using std::min;
 
-enum loss_t
-{ ENTROPY	= 1,
-  EUCLIDEAN	= 2,
-  LOGISTIC	= 3
+enum loss_t {
+    CLASSIFICATION = 1,
+    SEGMENT = 2,
+    EUCLIDEAN = 3
 };
 
 #ifdef __CUDACC__
-template LayerLoss<GPU>::LayerLoss (ParaLayer &pl, const int did, TensorGPUf &src, TensorGPUf &dst);
+template LayerLoss<GPU>::LayerLoss (ParaLayer& pl, const int did, TensorGPUf& src, TensorGPUf& dst);
 #else
-template LayerLoss<CPU>::LayerLoss (ParaLayer &pl, const int did, TensorCPUf &src, TensorCPUf &dst);
+template LayerLoss<CPU>::LayerLoss (ParaLayer& pl, const int did, TensorCPUf& src, TensorCPUf& dst);
 #endif
 
-template <typename DT>
-XPU_KERNEL(SigmoidForward) (
-  const int num_kernels, const DT* src_data, DT* dst_data)
-{ kernel_for (i, num_kernels)
-    dst_data[i] = (DT)1. / ((DT)1. + exp(min(-src_data[i], (DT)32.)));
-};
-
-LAYER_FORWARD (LayerLoss)
-{ const int N = dst_.size();
-  float  sasum = 0;
-  src_.blas_asum (sasum);
-  if (sasum >= 1e10 || isnan (sasum))
-    LOG (FATAL) << "\tXPU  " << did_ << "\tsasum is too large";
-  if (sasum <= 1e-7)
-    LOG (FATAL) << "\tXPU  " << did_ << "\tsasum is too small";
-  switch (pl_.loss)
-  { case ENTROPY:  // p(c|x)
-#ifdef __CUDACC__
-      cuda_check (cudnnSoftmaxForward (CUDNN_HANDLE, CUDNN_SOFTMAX_LOG, CUDNN_SOFTMAX_MODE_INSTANCE,
-        &alpha, srcDesc_, src_.dptr,
-       	&beta,  dstDesc_, src_.dptr));
-      src_.blas_vexp (src_);
-#else
-      for (int i = 0; i < src_.nums(); ++i)
-        src_[i].softmax ();
-#endif
-      break;
-    case EUCLIDEAN:
-      break;
-    case LOGISTIC:  // p(c|x)
-      XPU_KERNEL_LAUNCH (SigmoidForward,  cuda_get_blocks(N), CUDA_NUM_THREADS, 0, CUDNN_STREAM,
-      N, src_.dptr, src_.dptr);
-      cuda_sync_check ("SigmoidForward");
-      break;
-    default:
-      LOG (FATAL) << "not implemented loss method";
-  }
+template <typename XPU>
+void LayerLoss<XPU>::fprop (const bool is_train, const bool is_fixed) {
+    float sasum = src_.blas_asum();
+    if (sasum >= 1e10)
+        LOG (FATAL) << "\tXPU\t" << did_ << "\tsasum is too large";
+    if (sasum <= 1e-8)
+        LOG (FATAL) << "\tXPU\t" << did_ << "\tsasum is too small";
+    if (std::isnan (sasum))
+        LOG (FATAL) << "\tXPU\t" << did_ << "\tsasum is nan";
+    if (std::isinf (sasum))
+        LOG (FATAL) << "\tXPU\t" << did_ << "\tsasum is inf";
+    switch (pl_.type) {
+        case CLASSIFICATION:  // p(c|x)
+            src_.softmax();
+            break;
+        case SEGMENT:  // p(c|x)
+            src_.sigmoid();
+            aux_.binary_loss (src_, dst_);
+            mid_.reduce_sum (aux_);
+            mid_.blas_scal (1.f/area_);
+            break;
+        case EUCLIDEAN:
+            break;
+        default:
+            LOG (FATAL) << "not implemented loss method";
+    }
 }
 
-LAYER_BACKPROP (LayerLoss)
-{ float  gasum = 0;
-  switch (pl_.loss)
-  { case ENTROPY:  // p(c|x) - 1(y == c)
-      src_.blas_axpy (dst_, -1);
-      break;
-    case EUCLIDEAN:
-      src_.blas_axpy (dst_, -1);
-      break;
-    case LOGISTIC:  // dp/dx = p*(1-p)
-      src_.blas_axpy (dst_, -1);
-      break;
-    default:
-      LOG (FATAL) << "not implemented loss method";
-  }
-  src_.blas_scal (1.f/nums_);
-  src_.blas_asum (gasum);
-  if (gasum >= 1e10 || isnan (gasum))
-    LOG (FATAL) << "\tXPU  " << did_ << "\tgasum is too large";
-  if (gasum <= 1e-7)
-    LOG (FATAL) << "\tXPU  " << did_ << "\tgasum is too small";
+template <typename XPU>
+void LayerLoss<XPU>::bprop (const bool is_prop_grad) {
+    switch (pl_.type) {
+        case CLASSIFICATION:  // p(c|x) - 1(y == c)
+            src_.blas_axpy (aux_, -pl_.drop);
+            src_.blas_axpy (dst_, +pl_.drop-1);
+            break;
+        case SEGMENT:
+            src_.blas_axpy (dst_, -1);
+            break;
+        case EUCLIDEAN:
+            src_.blas_axpy (dst_, -1);
+            break;
+        default:
+            LOG (FATAL) << "not implemented loss method";
+    }
+    src_.blas_scal (1.f/nums_/area_);
+    float gasum = src_.blas_asum();
+    if (gasum >= 1e10)
+        LOG (FATAL) << "\tXPU\t" << did_ << "\tgasum is too large";
+    if (gasum <= 1e-8)
+        LOG (FATAL) << "\tXPU\t" << did_ << "\tgasum is too small";
+    if (std::isnan (gasum))
+        LOG (FATAL) << "\tXPU\t" << did_ << "\tgasum is nan";
+    if (std::isinf (gasum))
+        LOG (FATAL) << "\tXPU\t" << did_ << "\tgasum is inf";
 }
 
-LAYER_INIT (LayerLoss)
-{ nums_ = src_.nums();
-  dims_ = src_.size() / nums_;
+template <typename XPU>
+void LayerLoss<XPU>::init_layer () {
+    chls_ = src_.chls();
+    dims_ = src_.dims();
+    nums_ = src_.nums();
+    area_ = src_.area();
 
-  dst_.create (src_.shape, did_);
+    Shape eval_shape (1, 1, chls_, nums_);
+    mid_.create (eval_shape, did_);  // TODO
+    dst_.create (src_.shape, did_);
+    aux_.create (src_.shape, did_);
+    aux_.init (1.f/dims_);
 #ifdef __CUDACC__
-  cuda_check (cudnnCreateTensorDescriptor (&srcDesc_));
-  cuda_check (cudnnCreateTensorDescriptor (&dstDesc_));
-  src_.setTensor4dDesc (srcDesc_);
-  dst_.setTensor4dDesc (dstDesc_);
+    cuda_check (cudnnCreateTensorDescriptor (&srcDesc_));
+    cuda_check (cudnnCreateTensorDescriptor (&dstDesc_));
+    src_.setTensor4dDesc (srcDesc_);
+    dst_.setTensor4dDesc (dstDesc_);
 #endif
 }
 
